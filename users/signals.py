129294195d9e9392b216874e63a330185cdc406a
Isaac Cheng <isaac.cheng97@gmail.com>
2023-02-26 16:35:31 -0700

    Implemented profile page, allow updating account

---
+++ users/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+# When a user is saved, send this signal and this reciever (create_profile) takes these arguments and
+# instance -> the User instance
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
