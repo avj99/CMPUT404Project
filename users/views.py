c77ebaf9bec3d9830f545f8863e2f0ecd15acce4
Isaac Cheng <isaac.cheng97@gmail.com>
2023-02-25 19:10:32 -0700

    Create user app, implement register page

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+'''
+messages.debug
+messages.info
+messages.success
+messages.warning
+messages.error
+'''
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}")
+            return redirect("stream-home")
+
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'users/register.html', {"form": form})
+
