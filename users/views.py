b47a9621628d9840d943e11888ecb5e02db49df5
Isaac Cheng <isaac.cheng97@gmail.com>
2023-02-26 07:25:34 -0700

    Implement login and logout page, nagivation bar that works with auth

--- users/views.py
+++ users/views.py
@@ -16,11 +16,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}")
-            return redirect("stream-home")
-
+            messages.success(request, f"Your account has been created! You are now able to login.")
+            return redirect("login")
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {"form": form})
+
+
 
