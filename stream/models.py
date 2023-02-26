480973c64cf4e7d7adab0f064e31c93296ca01f7
Isaac Cheng <isaac.cheng97@gmail.com>
2023-02-25 18:24:55 -0700

    Update timezone, no longer display dummy data

--- stream/models.py
+++ stream/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()                               # TextField can have more than 255 characters
+    date_posted = models.DateTimeField(default=timezone.now) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # If user is deleted, all his/her posts are deleted
+
+    def __str__(self):
+        return self.title
+
