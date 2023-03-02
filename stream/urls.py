a12cc37392356ed74ff028398f28950e48f827bb
Javin Vora <javinvora@Javins-MacBook-Air.local>
2023-03-01 20:13:05 -0700

    Done with the Comment parts comepletely

--- stream/urls.py
+++ stream/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='stream-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # path('post/<int:pk>/liked',post_liked, name='post-liked'), 
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='post-comments'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='stream-about'),
 ]
