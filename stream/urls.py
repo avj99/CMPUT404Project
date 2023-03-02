8086071a930e1f5478c9a12bb051344bb38fe275
ZhiyuanYu133 <zyu6@ualberta.ca>
2023-03-03 01:01:48 +0800

    Add Friend Model and Post CURD operation

--- stream/urls.py
+++ stream/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='stream-about'),
-    path('welcome/', views.welcome, name = 'welcome')
+    path('friends/', views.friends, name='stream-friends'),
+    path('friends/find', views.friends_find, name='stream-friends-find'),
+    path('friends/add', views.friends_add, name='stream-friends-add'),
+    path('friends/del', views.friends_del, name='stream-friends-del'),
+    path('friends/success', views.friends_success, name='stream-friends-success'),
+    path('friends/refuse', views.friends_refuse, name='stream-friends-refuse'),
 ]
--- stream/views.py
+++ stream/views.py
@@ -1,10 +1,11 @@
+from time import time
+
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
+from .models import Post, Friends
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.decorators import user_passes_test, login_required
-from django.utils.decorators import method_decorator
-from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 '''
 Example Post Format
@@ -16,33 +17,29 @@ Example Post Format
 }
 '''
 
+
 # render(request, page to render, context)
 
-@user_passes_test(lambda u: not u.is_authenticated, login_url='stream-home')
-def welcome(request):
-    return render(request, "stream/welcome.html")
-
-
-@login_required
 def home(request):
     context = {
         "posts": Post.objects.all()
     }
+
     return render(request, "stream/home.html", context)
 
+
 # See if we can filter post here
-@method_decorator(login_required(login_url=reverse_lazy('welcome')), name='dispatch')
 class PostListView(ListView):
     model = Post
     template_name = "stream/home.html"
     context_object_name = "posts"
     ordering = ['-date_posted']
 
-@method_decorator(login_required(login_url=reverse_lazy('welcome')), name='dispatch')
+
 class PostDetailView(DetailView):
     model = Post
 
-@method_decorator(login_required(login_url=reverse_lazy('welcome')), name='dispatch')
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ["title", "content"]
@@ -51,8 +48,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
-@method_decorator(login_required(login_url=reverse_lazy('welcome')), name='dispatch')
+
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ["title", "content"]
@@ -61,7 +58,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     # Check if user browsing the post is the author
     def test_func(self):
         post = self.get_object()
@@ -70,7 +67,6 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return False
 
 
-@method_decorator(login_required(login_url=reverse_lazy('welcome')), name='dispatch')
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
 
@@ -82,8 +78,158 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
-    
-@login_required
+
+
 def about(request):
     return render(request, "stream/about.html", {'title': 'About'})
 
+
+def friends(request):
+    res2 = Friends.objects.filter(uid=request.user.id, is_new=2).all()
+    res2_1 = Friends.objects.filter(friend_id=request.user.id, is_new=2).all()
+
+    post3temp = []
+    for i in res2:
+        fid = i.friend_id
+        i.friend_id = User.objects.filter(id=i.friend_id).all()[0]
+        i.uid = User.objects.filter(id=fid).all()[0]
+        post3temp.append(i)
+    print(post3temp)
+    #
+    for i in res2_1:
+        uidt = i.friend_id
+        i.uid = User.objects.filter(id=i.uid).all()[0]
+        i.friend_id = User.objects.filter(id=uidt).all()[0]
+        post3temp.append(i)
+    print(post3temp)
+
+    res = Friends.objects.filter(friend_id=request.user.id, is_new=1).all()
+    post2temp = []
+    for i in res:
+        i.friend_id = User.objects.filter(id=i.friend_id).all()[0]
+        i.uid = User.objects.filter(id=i.uid).all()[0]
+        post2temp.append(i)
+    print(post2temp)
+
+    return render(request, "stream/friends.html",
+                  {'title': 'Friends', 'friends': post3temp,
+                   'posts2': post2temp})
+
+
+def friends_find(request):
+    id = request.GET.get("id")
+    if id == "":
+        return render(request, "stream/error.html",
+                      {'title': 'The friend ID you entered is empty and cannot be queried'})
+
+    if str(id) == str(request.user.id):
+        return render(request, "stream/error.html",
+                      {'title': "You can't add yourself as a friend"})
+
+    print(User.objects.filter(id=id).all())
+    res = User.objects.filter(id=id).all()
+    if not User.objects.filter(id=id).all():
+        return render(request, "stream/error.html",
+                      {'title': "This ID can't be found. You can't add friends"})
+
+    return render(request, "stream/friends_find_info.html",
+                  {'title': 'Friends', 'friends': User.objects.filter(id=id).all()})
+
+
+def friends_add(request):
+    id = request.GET.get("id")
+    if id == "":
+        return render(request, "stream/error.html",
+                      {'title': 'The friend ID you entered is empty and cannot be queried'})
+
+    if str(id) == str(request.user.id):
+        return render(request, "stream/error.html",
+                      {'title': "You can't add yourself as a friend"})
+
+    print(User.objects.filter(id=id).all())
+    res = User.objects.filter(id=id).all()
+    if not User.objects.filter(id=id).all():
+        return render(request, "stream/error.html",
+                      {'title': "This ID can't be found. You can't add friends"})
+    # 查询是否已经是加过的好友
+    if not Friends.objects.filter(uid=request.user.id, friend_id=id).all():
+        # 可以添加
+        Friends.objects.create(uid=request.user.id, friend_id=id, is_new=1, join_date=time())
+    else:
+        return render(request, "stream/error.html",
+                      {'title': "He is already your friend or The other party hasn't passed your friend request !!"})
+    return render(request, "stream/success.html",
+                  {'title': 'Add Friend Success , Please wait for the other party to pass!', 'url': '/friends'})
+
+
+def friends_del(request):
+    id = request.GET.get("id")
+    if id == "":
+        return render(request, "stream/error.html",
+                      {'title': 'The friend ID you entered is empty and cannot be queried'})
+
+    if str(id) == str(request.user.id):
+        return render(request, "stream/error.html",
+                      {'title': "You can't del yourself as a friend"})
+
+    if not User.objects.filter(id=id).all():
+        return render(request, "stream/error.html",
+                      {'title': "This ID can't be found. You can't add friends"})
+    # 查询是否已经是加过的好友
+    if Friends.objects.filter(
+            Q(friend_id=id) | Q(uid=request.user.id) | Q(uid=id) | Q(friend_id=request.user.id)).all():
+        # 可以删除
+        Friends.objects.filter(id=Friends.objects.filter(
+            Q(friend_id=id) | Q(uid=request.user.id) | Q(uid=id) | Q(friend_id=request.user.id)).all().first().id).delete()
+    else:
+        return render(request, "stream/error.html",
+                      {'title': "He is not your friend !!"})
+    return render(request, "stream/success.html", {'title': 'Remove Friend Success!', 'url': '/friends'})
+
+
+def friends_success(request):
+    id = request.GET.get("id")
+    if id == "":
+        return render(request, "stream/error.html",
+                      {'title': 'The friend ID you entered is empty and cannot be queried'})
+
+    if str(id) == str(request.user.id):
+        return render(request, "stream/error.html",
+                      {'title': "You can't del yourself as a friend"})
+
+    if not User.objects.filter(id=id).all():
+        return render(request, "stream/error.html",
+                      {'title': "This ID can't be found. You can't add friends"})
+    # 查询是否已经是加过的好友
+    if Friends.objects.filter(friend_id=request.user.id, uid=id).all():
+        # 可以更新
+        Friends.objects.filter(uid=id, friend_id=request.user.id).update(is_new=2)
+    else:
+        return render(request, "stream/error.html",
+                      {'title': "He is not your friend !!"})
+    return render(request, "stream/success.html", {'title': 'Success Friend Success!', 'url': '/friends'})
+
+
+def friends_refuse(request):
+    id = request.GET.get("id")
+    if id == "":
+        return render(request, "stream/error.html",
+                      {'title': 'The friend ID you entered is empty and cannot be queried'})
+
+    if str(id) == str(request.user.id):
+        return render(request, "stream/error.html",
+                      {'title': "You can't del yourself as a friend"})
+
+    print(User.objects.filter(id=id).all())
+    res = User.objects.filter(id=id).all()
+    if not User.objects.filter(id=id).all():
+        return render(request, "stream/error.html",
+                      {'title': "This ID can't be found. You can't add friends"})
+    # 查询是否已经是加过的好友
+    if Friends.objects.filter(friend_id=request.user.id, uid=id).all():
+        # 可以up
+        Friends.objects.filter(uid=id, friend_id=request.user.id).delete()
+    else:
+        return render(request, "stream/error.html",
+                      {'title': "He is not your friend !!"})
+    return render(request, "stream/success.html", {'title': 'Refuse Friend Success!', 'url': '/friends'})
