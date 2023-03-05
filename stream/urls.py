a33b2764182ed7f7a0c69df0d9de672b20f31f69
Isaac Cheng <isaac.cheng97@gmail.com>
2023-03-05 12:03:15 -0700

    Revert "Add Friend Model and Post CURD operation"
    This reverts commit 8086071a930e1f5478c9a12bb051344bb38fe275.

--- stream/urls.py
+++ stream/urls.py
@@ -9,10 +9,5 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='stream-about'),
-    path('friends/', views.friends, name='stream-friends'),
-    path('friends/find', views.friends_find, name='stream-friends-find'),
-    path('friends/add', views.friends_add, name='stream-friends-add'),
-    path('friends/del', views.friends_del, name='stream-friends-del'),
-    path('friends/success', views.friends_success, name='stream-friends-success'),
-    path('friends/refuse', views.friends_refuse, name='stream-friends-refuse'),
+    path('welcome/', views.welcome, name = 'welcome')
 ]
