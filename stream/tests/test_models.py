cbdd09993715117a3003ffe7ee7121c9f306006c
avj99 <jadhav@DESKTOP-HH4FJR4>
2023-03-08 16:45:02 -0700

    testing on stream done

---
+++ stream/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from stream.models import Post, Comment 
+from django.contrib.auth.models import User
+import tempfile
+
+class testModels(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="test1",
+                                    email="test1@domain.com",
+                                    password="password1234567890")
+        self.post1= Post.objects.create(
+            title = 'test Title',
+            content = 'test content',
+            #date_posted = date,
+            image = tempfile.NamedTemporaryFile(suffix=".jpg").name,
+            author= self.user
+            )
+        self.comment1= Comment.objects.create(
+            main_post = self.post1,
+            name = 'test name',
+            body = 'test body',
+            # main_date =
+        )
+    def test_Post(self):
+        self.assertEquals(self.post1.author,self.user)
+
+    def test_Comment(self):
+        self.assertEquals(self.comment1.main_post,self.post1)
