8f839be0fca60f393e85ad49c3a718d4ab4804cc
Isaac Cheng <isaac.cheng97@gmail.com>
2023-03-09 01:22:16 -0700

    Fix redirecting to API page problem

--- social_distribution/urls.py
+++ social_distribution/urls.py
@@ -73,13 +73,15 @@ router.register(r'profile', ProfileViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('stream.urls')),
     path("register/", user_views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('profile/', user_views.profile, name="profile"),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # Keep this path at the bottom!
+    path("", include('stream.urls')), 
 ] 
 
 # For debugging, do not touch!
